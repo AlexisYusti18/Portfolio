ba18550921a642e893e48e2c79d1de9c1519cee6
Alexis Yusti <alexisyusti18@gmail.com>
2022-08-16 22:20:23 -0300

    me

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import photo from '../assets/photo.png'
 import pdf from '../assets/docu.pdf'
+import logoLinkedin from '../assets/logoLinkedin.png'
 
 export default function Home(){
   return (
     <div className="home">
       <div className='home-ctn'>
-        <div>
+        <div className='about-ctn'>
           <h1>I'm Alexis Yusti</h1>
           <p>MERN Fullstack & Mobile Apps</p>
           <a className='btn-cv' href={pdf} download={' '}>Download CV</a>
+          <a href="https://www.linkedin.com/in/alexisyusti" className='linkedin-btn'>
+            Linkedin
+          </a>
         </div>
         <div>
           <img className="photo" src={photo} alt="Alexis"/>
